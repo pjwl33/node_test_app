944d228771811c7435a743ecd95ff218aba1a80c
pjwl33 <paul.leejw@gmail.com>
2014-06-05 02:11:07 -0400

    refactored

--- hello.js
+++ hello.js
@@ -2,10 +2,11 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(req, resp) {
-  resp.writeHead(200);
-  fs.readFile('index.html', function(err, cont) {
-    resp.write(cont);
-    resp.end();
+  resp.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  fs.readFile('index.html', function(err, content) {
+    resp.end(content);
   });
 }).listen(8080);
 
