df9ceb8c93acdafb4c1294644e23983b557d20a1
pjwl33 <paul.leejw@gmail.com>
2014-06-05 02:26:15 -0400

    yay node

--- hello.js
+++ hello.js
@@ -1,13 +1,29 @@
-var http = require('http');
+// var http = require('http');
+// http.createServer(function(req, resp) {
+//   resp.writeHead(200, {
+//     'Content-Type': 'text/html'
+//   });
+//   fs.readFile('index.html', function(err, content) {
+//     resp.end(content);
+//   });
+// }).listen(8080);
+
+// console.log('Listening to port 8080...');
+
+var express = require('express');
+var logfmt = require('logfmt');
+var app = express();
 var fs = require('fs');
 
-http.createServer(function(req, resp) {
-  resp.writeHead(200, {
-    'Content-Type': 'text/html'
-  });
-  fs.readFile('index.html', function(err, content) {
-    resp.end(content);
-  });
-}).listen(8080);
+app.use(logfmt.requestLogger());
 
-console.log('Listening to port 8080...');
+app.get('/', function(req, resp) {
+  fs.readFile('index.html', function(err, content) {
+    resp.write(content);
+  });
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log('Listening on ' + port);
+});
