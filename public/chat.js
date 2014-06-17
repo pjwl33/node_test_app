c5f778fcfa5b4888e08698cef64526828c9ab8af
pjwl33 <paul.leejw@gmail.com>
2014-06-17 16:40:18 -0400

    port change: gs

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
   var messages = [];
-  var socket = io.connect('http://localhost:5000' || 'http://paulsnodejs.herokuapp.com/');
+  var socket = io.connect('http://paulsnodejs.herokuapp.com/');
   var field = document.getElementById("field");
   var sendButton = document.getElementById("send");
   var content = document.getElementById("content");
