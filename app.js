d2a7a46bfb13394b8bef8be33f6b6f7af2c475f4
pjwl33 <paul.leejw@gmail.com>
2014-06-17 16:33:41 -0400

    update [port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var logfmt = require('logfmt');
 var app = express();
-var port = Number(process.env.PORT || 8080);
+var port = Number(process.env.PORT || 5000);
 
 app.use(logfmt.requestLogger());
 app.set('views', __dirname + '/tpl');
